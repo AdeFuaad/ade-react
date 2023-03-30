364b8fb39dffab5f8c28854df42efa54f04be794
AdeFuaad <adesholafuaad@gmail.com>
2023-03-30 12:27:58 -0400

    dependencies bug

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
-import Header from './components/Header/Header';
-import AboutMe from './components/AboutMe/AboutMe';
-import Portfolio from './components/Portfolio/Portfolio';
-import Contact from './components/Contact/Contact';
-import Resume from './components/Resume/Resume';
-import Footer from './components/Footer/Footer';
-import './App.scss';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Switch,
+} from "react-router-dom";
+import Header from "./components/Header/Header";
+import AboutMe from "./components/AboutMe/AboutMe";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Contact from "./components/Contact/Contact";
+import Resume from "./components/Resume/Resume";
+import Footer from "./components/Footer/Footer";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="content">
-        <Switch>
-          <Route exact path="/" component={AboutMe} />
-          <Route path="/portfolio" component={Portfolio} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/resume" component={Resume} />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<AboutMe />} index />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
       </div>
       <Footer />
     </div>
